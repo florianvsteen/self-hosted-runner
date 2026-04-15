acd217fae9479118bc775ab8fae3d13206061a09
florianvsteen <florian@flownet.be>
2026-04-15 15:34:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
 REPO=$REPO
 REG_TOKEN=$REG_TOKEN
 NAME=$NAME
+
+# Start Docker daemon
+sudo dockerd &
+
+# Wait for Docker to be ready
+until docker info >/dev/null 2>&1; do
+    echo "Waiting for Docker daemon..."
+    sleep 1
+done
+echo "Docker daemon is running"
 
 cd /home/docker/actions-runner || exit
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
